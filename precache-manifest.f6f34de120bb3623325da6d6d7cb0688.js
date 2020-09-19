c43b64d0140b6c79220d33bdd0ed6df767be6249
kabdelka <ksabdelka@gmail.com>
2020-09-19 21:17:20 +0100

    Updates

---
+++ precache-manifest.f6f34de120bb3623325da6d6d7cb0688.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e81614986f8e0f255dac2826a498aa",
+    "url": "/apprecipe/index.html"
+  },
+  {
+    "revision": "0446bd0c83fc89519c72",
+    "url": "/apprecipe/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "72f8ec535786c9427b4d",
+    "url": "/apprecipe/static/css/main.c2f42479.chunk.css"
+  },
+  {
+    "revision": "0446bd0c83fc89519c72",
+    "url": "/apprecipe/static/js/2.dedbbb4c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/apprecipe/static/js/2.dedbbb4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72f8ec535786c9427b4d",
+    "url": "/apprecipe/static/js/main.c0ecd3ce.chunk.js"
+  },
+  {
+    "revision": "a25a7192401f71a1a1a1",
+    "url": "/apprecipe/static/js/runtime-main.e132d26c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/apprecipe/static/media/logo.5d5d9eef.svg"
+  }
+]);
